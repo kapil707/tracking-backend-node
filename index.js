23ca981ffb67535ffe19e0ed465b8080322fd91d
Kapil <kapil@Kapils-Mac-mini.local>
2026-03-07 13:38:37 +0530

    add

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const io = new Server(server, { cors: { origin: "*" } });
 //Middleware - plugin
 //app.use(express.urlencoded({extended:false}));
 
-app.use(logReqRes("log.txt"));
+//app.use(logReqRes("log.txt"));
 
 app.use(cors());
 app.use(express.json());

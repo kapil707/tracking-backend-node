7ebefbd150377b7ee558ea21c20ffb72c28bd018
Kapil <kapil@Kapils-Mac-mini.local>
2026-03-09 14:08:00 +0530

    add

--- routes/trackingRoutes.js
+++ routes/trackingRoutes.js
@@ -4,6 +4,6 @@ const trackingController = require('../controllers/trackingController');
 
 // Hum controller ko 'io' pass karne ke liye callback function use karenge
 module.exports = (io) => {
-    router.post('/log-location', (req, res) => trackingController.postLocation(req, res, io));
+    router.post('/log-location', (req, res) => trackingController.insertme(req, res, io));
     return router;
 };

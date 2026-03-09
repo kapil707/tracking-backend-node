6d8b8ee40820b71adc0207de941cbbc13cf1e079
Kapil <kapil@Kapils-Mac-mini.local>
2026-03-09 13:59:03 +0530

    add

--- controllers/trackingController.js
+++ controllers/trackingController.js
@@ -1,25 +1,25 @@
 const Tracking = require('../models/trackingModel');
 
 exports.postLocation = async (req, res, io) => {
-    try {
+    //try {
         const { orderId, lat, lng } = req.body;
         
         // Debugging ke liye log lagayein
         console.log("Data received:", req.body);
 
-        if (!orderId || !lat || !lng) {
-            return res.status(400).json({ error: "Missing orderId, lat, or lng" });
-        }
+    //     if (!orderId || !lat || !lng) {
+    //         return res.status(400).json({ error: "Missing orderId, lat, or lng" });
+    //     }
 
-        // DB mein save karein
-        await Tracking.saveLocation(orderId, lat, lng);
+    //     // DB mein save karein
+    //     await Tracking.saveLocation(orderId, lat, lng);
         
-        // Live update bhejein
-        io.emit(`locationUpdate-${orderId}`, { orderId, lat, lng });
+    //     // Live update bhejein
+    //     io.emit(`locationUpdate-${orderId}`, { orderId, lat, lng });
 
-        return res.status(200).json({ status: "Success", message: "Location Logged" });
-    } catch (err) {
-        console.error("Controller Error:", err.message);
-        return res.status(500).json({ error: "Database or Server Error" });
-    }
+    //     return res.status(200).json({ status: "Success", message: "Location Logged" });
+    // } catch (err) {
+    //     console.error("Controller Error:", err.message);
+    //     return res.status(500).json({ error: "Database or Server Error" });
+    // }
 };
